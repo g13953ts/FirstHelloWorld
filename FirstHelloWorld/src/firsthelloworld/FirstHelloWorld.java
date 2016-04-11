05203f0aac5b611d9dc1f1969e6e672f08e4f4be
g13953ts <g13953ts@m20215.ACTSUDA>
2016-04-11 14:18:42 +0900

    add a project

---
+++ FirstHelloWorld/src/firsthelloworld/FirstHelloWorld.java
@@ -0,0 +1,10 @@
+package firsthelloworld;
+
+public class FirstHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
